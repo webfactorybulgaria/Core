a9b0cb69b42cc1f273f8626a36e98b697afb3dd7
Nasko <nasko@webfactory.bg>
2016-05-10 12:02:30 +0300

    introducing PublicNavigator

--- src/Http/Controllers/BasePublicController.php
+++ src/Http/Controllers/BasePublicController.php
@@ -3,14 +3,25 @@
 namespace TypiCMS\Modules\Core\Http\Controllers;
 
 use Illuminate\Routing\Controller;
+use TypiCMS\Modules\Core\Services\PublicNavigator;
 
 abstract class BasePublicController extends Controller
 {
     protected $repository;
+    public $module;
 
-    public function __construct($repository = null)
+    public function __construct($repository = null, $module = null)
     {
         $this->middleware('public');
         $this->repository = $repository;
+        $this->module = $module;
+
+        /*
+        |--------------------------------------------------------------------------
+        | Navigation utilities.
+        |--------------------------------------------------------------------------
+        */
+        app()->instance('public.navigator', new PublicNavigator($this));
+
     }
 }
---
+++ src/Services/PublicNavigator.php
@@ -0,0 +1,54 @@
+<?php
+namespace TypiCMS\Modules\Core\Services;
+
+use Illuminate\Support\Facades\Request;
+
+class PublicNavigator
+{
+    private $controller;
+    public function __construct($controller)
+    {
+        $this->controller = $controller;
+    }
+
+    public function currentModule()
+    {
+        return $this->controller->module;
+    }
+
+    public function currentPage()
+    {
+        if (empty($this->currentModule())) return null;
+
+        if ($this->currentModule() != 'page') {
+            app()->instance('currentPage', app('typicms')->getPageLinkedToModule($this->currentModule()));
+        }
+
+        return app('currentPage');
+    }
+    
+    /**
+    * Get array with IDs of pages (path to root)
+    *
+    * @return - array
+    *
+    */
+    public function pathToRoot()
+    {
+        $eloquentPage = app('TypiCMS\Modules\Pages\Repositories\PageInterface');
+        $arrParents = $eloquentPage->allForTreeMap();
+
+        $path = [];
+
+        if ($currentPage = $this->currentPage()) {
+            $id = (int) $currentPage->id;
+            $path[] = $id;
+            while (!empty($arrParents[$id])) {
+                $path[] = (int) $arrParents[$id];
+                $id = (int) $arrParents[$id];
+            }
+        }
+
+        return $path;
+    }
+}
